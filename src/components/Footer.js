f1fa9c810f662914208a49d14373ee36e7f435ab
mohitkumar402 <mkfunmasti@gmail.com>
2025-04-05 23:23:39 +0530

    Initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by mohit kumar</h3>
+          <h3>Designed and Developed by mohit kumar contact 6203802704</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} mohit</h3>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,8 @@ import Particle from "../Particle";
 import chatify from "../../Assets/Projects/image.png";
 
 import bitsOfCode from "../../Assets/Projects/blog.png";
+//import bitsOfCode1 from "../../Assets/Projects/bg2.jpg";
+import suicide from "../../Assets/Projects/bg2.jpg";
 
 function Projects() {
   return (
@@ -39,10 +41,33 @@ function Projects() {
               title="personal portfolio"
               description="A sleek and fully responsive personal portfolio website showcasing my skills, projects, and experience as a Full-Stack Developer. Built using React.js, Node.js, and MongoDB, this portfolio highlights my expertise in web development, includes a dynamic project showcase, and features smooth navigation and interactive elements."
               ghLink="https://github.com/mohitkumar402/mohit-kumar-portfolio"
+              demoLink="https://mohit-kumar-portfolio.vercel.app/"
               
             />
           </Col>
 
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={suicide}
+              isBlog={false}
+              title="weather App"
+              description="A weather app that provides real-time weather updates and forecasts for any location. Users can search for their city and view current conditions, hourly forecasts, and extended weather predictions. The app is built using React.js and integrates with a weather API to deliver accurate and up-to-date information."
+              //description="A sleek and fully responsive personal portfolio website showcasing my skills, projects, and experience as a Full-Stack Developer. Built using React.js, Node.js, and MongoDB, this portfolio highlights my expertise in web development, includes a dynamic project showcase, and features smooth navigation and interactive elements."
+              ghLink="https://github.com/mohitkumar402/weatherapp.git"
+              demoLink="https://mohit-kumar402.vercel.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={suicide}
+              isBlog={false}
+              title="tradersplayground"
+              description="crypto app tp trade on sample demo or papermoney."
+              //description="A sleek and fully responsive personal portfolio website showcasing my skills, projects, and experience as a Full-Stack Developer. Built using React.js, Node.js, and MongoDB, this portfolio highlights my expertise in web development, includes a dynamic project showcase, and features smooth navigation and interactive elements."
+              ghLink="https://github.com/mohitkumar402/tradersplayground.git"
+              demoLink="https://mohit-kumar402.vercel.app/"
+            />
+          </Col>
         </Row>
       </Container>
     </Container>
